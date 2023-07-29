a24dc7d41a8a5f9484efda175adece4e09d3479a
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-07-29 18:39:16 +0200

    Create Home.jsx

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../home/home.css'
+
+function Home() {
+  return (
+    <>
+    <section>
+        <div>
+            <p>hello world, this is a test</p>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Home
