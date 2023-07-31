d6c650cb12f50cf1668726e38aace0085bfaf5eb
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-07-31 23:31:59 +0200

    Added Responsivness to some home content

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,37 +4,40 @@ import {BsLinkedin} from 'react-icons/bs'
 import { BsGithub } from 'react-icons/bs'
 import {IoLogoWhatsapp} from 'react-icons/io'
 import {MdLightMode} from 'react-icons/md' 
-import homeImage from '../../assets/homeImage.png'
+import homeImage from '../../assets/homePicture1.png'
 
 function Home() {
   return (
     <>
     <section className='home'>
       <div className="homeContent container">
-        <div className="homeQuote grid">
-          "Hard Work Beats Talent When Talent <span className='quoteRed'> Doesn't Work Hard" </span>
-          </div>
-          <div className='icons grid'>
-            <BsLinkedin className="icon"/>
-            <BsGithub className="icon"/>
-            <IoLogoWhatsapp className="icon"/>
-            <MdLightMode className="icon"/>
-          </div>
-          <div className='btn-container grid'>
-            <button className='btn-cv'> Download CV </button>
-          </div>
-        <div className="textDiv grid">
-          <h1 className='textName'><b>Nathan</b> <span className='textSurname'>Davids</span> Nathan <span 
-          className='textSurname'>Davids</span>  Nathan <span 
-          className='textSurname'>Davids</span>  Nathan <span 
-          className='textSurname'>Davids</span>  Nathan <span 
-          className='textSurname'>Davids</span>  Nathan <span 
-          className='textSurname'>Davids</span>
+        
+        <div className="textDiv">
+          <h1 className='homeTitle'>
+            "Hard Work Beats Talent When Talent Doesn't Work Hard"
           </h1>
         </div>
-        <div className='mainPicture grid'>
-            <img src={homeImage} alt="" className='mainImg' />
-          </div>
+
+        <div className="icons grid">
+          <BsLinkedin className='icon'/>
+          <BsGithub className='icon'/>
+          <IoLogoWhatsapp className='icon'/>
+          <MdLightMode className='icon'/>
+        </div>
+
+        <div className="btn-container">
+          <button className='btn-cv'>Download CV</button>
+        </div>
+
+        <div className="cardDiv grid">
+          <h1 className='nameText flex'> Nathan <span className='surnameText'> Davids</span> Nathan
+          <span className='surnameText'> Davids </span> Nathan <span className='surnameText'>Davids</span>
+          Nathan <span className='surnameText'>Davids</span> Nathan <span>Davids</span>Nathan <span>Davids</span>
+          Nathan <span>Davids</span>
+          </h1>
+          <img src={homeImage} alt="" className='img' />
+        </div>
+
       </div>
     </section>
     </>
